154cc3334ea66862cfe6ee81eebffd5378ff17ee
Wufei Ma <wufeim@gmail.com>
2023-01-01 03:15:33 -0500

    add inference code for nemo_cls model

--- nemo/models/nemo_cls.py
+++ nemo/models/nemo_cls.py
@@ -28,6 +28,7 @@ class NeMoCls(BaseModel):
         max_group,
         down_sample_rate,
         mesh_path,
+        image_sizes,
         training,
         inference,
         checkpoint=None,
@@ -42,6 +43,7 @@ class NeMoCls(BaseModel):
         self.max_group = max_group
         self.down_sample_rate = down_sample_rate
         self.mesh_path = mesh_path
+        self.image_sizes = image_sizes
         self.training_params = training
         self.inference_params = inference
 
@@ -50,10 +52,18 @@ class NeMoCls(BaseModel):
         self.build()
 
     def build(self):
+        self.sanity_check()
         if self.mode == "train":
             self._build_train()
         else:
             self._build_inference()
+
+    def sanity_check():
+        # All image sizes should be the same
+        size_0 = self.image_sizes['aeroplane']
+        for cate in CATEGORIES:
+            assert size_0 == self.image_sizes[cate], \
+                "All image sizes should be the same for classification"
 
     def _build_train(self):
         self.n_gpus = torch.cuda.device_count()
@@ -101,7 +111,80 @@ class NeMoCls(BaseModel):
         self.pad_index = torch.tensor(self.pad_index, dtype=torch.long)
 
     def _build_inference(self):
-        pass
+        self.all_num_verts = []
+        assert '{:s}' in self.mesh_path, 'The mesh path should contain {:s} to format paths with categories'
+        for cate in CATEGORIES:
+            m_p = self.mesh_path.format(cate)
+            self.all_num_verts.append(load_off(m_p)[0].shape[0])
+
+        net = construct_class_by_name(**self.net_params)
+        self.net = nn.DataParallel(self.net).to(self.device)
+        self.net.load_state_dict(self.checkpoint["state"])
+
+        self.memory_bank = construct_class_by_name(
+            **self.memory_bank_params,
+            output_size=len(CATEGORIES)*max(self.all_num_verts)+self.num_noise*self.max_group,
+            num_pos=len(CATEGORIES)*max(self.all_num_verts),
+            n_list_set=self.all_num_verts,
+            num_noise=self.num_noise
+        ).to(self.device)
+        with torch.no_grad():
+            self.memory_bank.memory.copy_(checkpoint['memory'][0:memory_bank.memory.shape[0]])
+
+        image_h, image_w = self.image_sizes['aeroplane']
+        render_image_size = max(image_h, image_w) // self.down_sample_rate
+        map_shape = (image_h // self.down_sample_rate, image_w // self.down_sample_rate)
+
+        cameras = construct_class_by_name(
+            class_name=self.inference_params.cameras.class_name,
+            focal_length=self.inference_params.cameras.focal_length // self.down_sample_rate,
+            principal_point=((map_shape[1] // 2, map_shape[0] // 2),),
+            image_size=(map_shape,),
+            in_ndc=False,
+            device=self.device)
+        raster_settings = construct_class_by_name(
+            **self.inference_params.raster_settings, image_size=render_image_size
+        )
+        rasterizer = construct_class_by_name(
+            **self.inference_params.rasterizer, cameras=cameras, raster_settings=raster_settings
+        )
+
+        clutter = self.checkpoint["memory"][self.memory_bank.memory.shape[0] :].detach().cpu().numpy()
+        self.clutter_bank = torch.from_numpy(clutter).to(self.device)
+        self.clutter_bank = normalize_features(torch.mean(self.clutter_bank, dim=0)).unsqueeze(0)
+
+        self.inter_module, self.kp_features = {}, {}
+        for idx, cate in enumerate(CATEGORIES):
+            memory = self.checkpoint["memory"][idx*max(self.all_num_verts):(idx+1)*max(self.all_num_verts)].detach().cpu().numpy()
+            feature_bank = torch.from_numpy(memory)
+            self.kp_features[cate] = self.checkpoint["memory"][idx*max(self.all_num_verts):(idx+1)*max(self.all_num_verts)].to(self.device)
+            xvert, xface = load_off(self.mesh_path.format(cate), to_torch=True)
+            self.inter_module[cate] = MeshInterpolateModule(
+                xvert,
+                xface,
+                feature_bank,
+                rasterizer,
+                post_process=center_crop_fun(map_shape, (render_image_size,) * 2),
+            )
+
+        (
+            azimuth_samples,
+            elevation_samples,
+            theta_samples,
+            distance_samples,
+            px_samples,
+            py_samples,
+        ) = get_param_samples(self.cfg)
+
+        self.poses, self.kp_coords, self.kp_vis = {}, {}, {}
+        for cate in CATEGORIES:
+            self.poses[cate], self.kp_coords[cate], self.kp_vis[cate] = pre_compute_kp_coords(
+                self.mesh_path,
+                azimuth_samples=azimuth_samples,
+                elevation_samples=elevation_samples,
+                theta_samples=theta_samples,
+                distance_samples=distance_samples,
+            )
 
     def step_scheduler(self):
         self.scheduler.step()
@@ -165,6 +248,43 @@ class NeMoCls(BaseModel):
         self.loss_trackers['loss_reg'].append(loss_reg.item())
 
         return {'loss': loss.item(), 'loss_main': loss_main.item(), 'loss_reg': loss_reg.item()}
+
+    def evaluate(self, sample, debug=False):
+        self.net.eval()
+
+        sample = self.transforms(sample)
+        img = sample["img"].to(self.device)
+        assert len(img) == 1, "The batch size during validation should be 1"
+
+        with torch.no_grad():
+            feature_map = self.net.module.forward_test(img)
+
+        scores, preds = [], []
+        for cate in CATEGORIES:
+            self.inter_module[cate].to(self.device)
+            pred = solve_pose(
+                self.cfg,
+                feature_map,
+                self.inter_module[cate],
+                self.kp_features[cate],
+                self.clutter_bank,
+                self.poses[cate],
+                self.kp_coords[cate],
+                self.kp_vis[cate],
+                debug=debug,
+                device=self.device,
+            )
+            self.inter_module[cate].to('cpu')
+            preds.append(pred)
+            scores.append(pred['final'][0]['score'])
+
+        pred_cate = np.argmin(scores)
+        pred = preds[pred_cate]
+
+        if "azimuth" in sample and "elevation" in sample and "theta" in sample:
+            pred["pose_error"] = pose_error(sample, pred["final"][0])
+
+        return pred
 
     def get_ckpt(self, **kwargs):
         ckpt = {}
