9cc49e2eac299ececdf21af660d96cbdfade0413
Angtian Wang <63261150+Angtian@users.noreply.github.com>
2023-07-09 15:47:48 -0400

    Update general.py

--- nemo/utils/general.py
+++ nemo/utils/general.py
@@ -8,6 +8,38 @@ import numpy as np
 import torch
 
 
+def tensor_linspace(start, end, steps=10):
+    """
+    Vectorized version of torch.linspace.
+
+    Inputs:
+    - start: Tensor of any shape
+    - end: Tensor of the same shape as start
+    - steps: Integer
+
+    Returns:
+    - out: Tensor of shape start.size() + (steps,), such that
+      out.select(-1, 0) == start, out.select(-1, -1) == end,
+      and the other elements of out linearly interpolate between
+      start and end.
+    """
+    assert start.size() == end.size()
+    view_size = start.size() + (1,)
+    w_size = (1,) * start.dim() + (steps,)
+    out_size = start.size() + (steps,)
+
+    start_w = torch.linspace(1, 0, steps=steps).to(start)
+    start_w = start_w.view(w_size).expand(out_size)
+    end_w = torch.linspace(0, 1, steps=steps).to(start)
+    end_w = end_w.view(w_size).expand(out_size)
+
+    start = start.contiguous().view(view_size).expand(out_size)
+    end = end.contiguous().view(view_size).expand(out_size)
+
+    out = start_w * start + end_w * end
+    return out
+
+
 def get_pkg_root():
     root = os.path.dirname(os.path.abspath(__file__))
     root = os.path.abspath(os.path.join(root, ".."))
