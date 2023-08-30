37b994b1f652dc1da2205224223375949fa8af78
Wufei Ma <wufeim@gmail.com>
2023-08-30 15:55:35 -0400

    update create mesh code to customize categories

--- data/create_cuboid_mesh.py
+++ data/create_cuboid_mesh.py
@@ -97,11 +97,13 @@ def meshelize(x_range, y_range, z_range, number_vertices):
     return np.array(out_vertices), np.array(out_faces)
 
 
-def create_meshes(mesh_d, CAD_path, save_path, number_vertices, linear_coverage):
+def create_meshes(mesh_d, CAD_path, save_path, number_vertices, linear_coverage, categories=None):
+    if categories is None:
+        categories = CATEGORIES
     if mesh_d == 'single':
-        for cate in CATEGORIES:
+        for cate in categories:
             os.makedirs(os.path.join(save_path, cate), exist_ok=True)
-            fnames = [x for x in os.listdir(os.path.join(CAD_path, cate)) if x.endswith('.off')]
+            fnames = [x for x in os.listdir(os.path.join(CAD_path, cate)) if x.endswith('.off') and len(x) == 6]
 
             vertices = []
             for f in fnames:
