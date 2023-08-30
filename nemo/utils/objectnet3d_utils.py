65cf2457d2da8273ae4b34b531fcf3b55846b2eb
Wufei Ma <wufeim@gmail.com>
2023-08-30 15:31:20 -0400

    add data preprocessing for ObjectNet3D

---
+++ nemo/utils/objectnet3d_utils.py
@@ -0,0 +1,42 @@
+CATEGORIES = [
+    "bed",
+    "bookshelf",
+    "calculator",
+    "cellphone",
+    "computer",
+    "cabinet",
+    "guitar",
+    "iron",
+    "knife",
+    "microwave",
+    "pen",
+    "pot",
+    "rifle",
+    "slipper",
+    "stove",
+    "toilet",
+    "tub",
+    "wheelchair"
+]
+
+
+MESH_LEN = {
+    "bed": 10,
+    "bookshelf": 8,
+    "calculator": 5,
+    "cellphone": 11,
+    "computer": 12,
+    "cabinet": 15,
+    "guitar": 5,
+    "iron": 5,
+    "knife": 9,
+    "microwave": 6,
+    "pen": 7,
+    "pot": 7,
+    "rifle": 8,
+    "slipper": 6,
+    "stove": 6,
+    "toilet": 7,
+    "tub": 9,
+    "wheelchair": 5
+}
